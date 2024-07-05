4d6a1452a6c878e28f469d499044a42a45976215
jagernau <stepanenkov25@gmail.com>
2024-07-05 12:12:34 +0300

    сделал пагинацию МТС на метод структура абонента, добавил секрет для МТС НомерЛицевого счёта

--- mts_connect.py
+++ mts_connect.py
@@ -8,12 +8,21 @@ base_url = config.MTS_BASE_URL
 username = config.MTS_USERNAME
 password = config.MTS_PASSWORD
 parent_tel_number = config.MTS_PARENT_TEL_NUMBER
+account = config.MTS_ACCOUNT_NUMBER # Номер лицевого счёта
 
 class MtsApi:
-    def __init__(self, base_url, username, password):
+    def __init__(self, base_url, username, password, accountNo):
+        """ 
+        При инициализации принимает:
+        1 Базовый адрес
+        2 Логин в АПИ ЛК
+        3 Пароль в АПИ ЛК
+        4 Номер лицевого счёта
+        """
         self.base_url = base_url
         self.username = username
         self.password = password
+        self.accountNo = accountNo
         self.access_token = None
 
     def get_access_token(self):
@@ -47,12 +56,17 @@ class MtsApi:
         response = requests.post(url=url, headers=headers, data=data)
         return response.text
 
-    def get_structure_abonents(self):
+    def get_structure_abonents(self, pageNum: int):
         """ 
         Метод для получения структуры абонентов
+        Принимает:
+        1 Номер лицевого счёта self
+        2 Номер страницы
+        По умолчанию поставил количество результатов на странице: 10 
         :return: dict
+
         """
-        url = f"{self.base_url}/b2b/v1/Service/HierarchyStructure"
+        url = f"{self.base_url}/b2b/v1/Service/HierarchyStructure?account={int(self.accountNo)}&pageNum={int(pageNum)}&pageSize=10"
         headers = {
             "Content-Type": "application/json",
             "Authorization": f"Bearer {self.access_token}",
@@ -61,13 +75,13 @@ class MtsApi:
         return response.json()
 
 
-mts_api = MtsApi(base_url, username, password)
+mts_api = MtsApi(base_url, username, password, accountNo=account)
 mts_api.get_access_token()
 #all_sims = mts_api.get_all_sims(parent_tel_number)
-structure_abonents = mts_api.get_structure_abonents()
+structure_abonents = mts_api.get_structure_abonents(pageNum=5)
 print(structure_abonents)
 
-# with open('structure_abonents_mts.json', 'w', encoding='utf-8') as file:
-#     json.dump(structure_abonents, file, indent=2, ensure_ascii=False)
+with open('structure_abonents_mts_400.json', 'w', encoding='utf-8') as file:
+    json.dump(structure_abonents, file, indent=2, ensure_ascii=False)
 
 
